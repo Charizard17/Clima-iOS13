7fa2121e73876bd9fc6b69b2a035b06be93aee27
Charizard17 <3723esad@gmail.com>
2023-06-12 09:15:50 +0200

    url session and fetch data from the api

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,11 +9,38 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=API_KEY"
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=\(openWeatherApiKey)"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"
-        print("Api Key: \(openWeatherApiKey)")
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        // 1- Create Url
+        if let url = URL(string: urlString) {
+            
+            // 2- Create an URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3- Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            // 4- Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
