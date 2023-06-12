eb4bbaf58ca23ebc6bfdc70260191cb4af3dec6a
Charizard17 <3723esad@gmail.com>
2023-06-12 08:25:07 +0200

    .gitignore created again. Another test to check if it works

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -8,21 +8,6 @@
 
 import Foundation
 
-private var apiKey: String {
-  get {
-    // 1
-    guard let filePath = Bundle.main.path(forResource: "SECRET", ofType: "plist") else {
-      fatalError("Couldn't find file 'SECRET.plist'.")
-    }
-    // 2
-    let plist = NSDictionary(contentsOfFile: filePath)
-    guard let value = plist?.object(forKey: "API_KEY") as? String else {
-      fatalError("Couldn't find key 'API_KEY' in 'SECRET.plist'.")
-    }
-    return value
-  }
-}
-
 struct WeatherManager {
     let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=API_KEY"
     
