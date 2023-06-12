f2d53e12e0fa5ae0139a91c591f7be1363dcc5bd
Charizard17 <3723esad@gmail.com>
2023-06-12 08:00:15 +0200

    .gitignore and SECRET.plist files created. Test commit to check if SECRET file added to .gitignore

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -8,11 +8,27 @@
 
 import Foundation
 
+private var apiKey: String {
+  get {
+    // 1
+    guard let filePath = Bundle.main.path(forResource: "SECRET", ofType: "plist") else {
+      fatalError("Couldn't find file 'SECRET.plist'.")
+    }
+    // 2
+    let plist = NSDictionary(contentsOfFile: filePath)
+    guard let value = plist?.object(forKey: "API_KEY") as? String else {
+      fatalError("Couldn't find key 'API_KEY' in 'SECRET.plist'.")
+    }
+    return value
+  }
+}
+
 struct WeatherManager {
     let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=API_KEY"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"
+        print("Api Key: \(apiKey)")
         print(urlString)
     }
 }
